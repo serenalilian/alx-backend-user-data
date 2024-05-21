d0068d48ac6ed5bcf6442154e8d9b4910d837cd7
serenalilian <serenalilian99@gmail.com>
2024-05-21 18:51:07 +0000

    corrers

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -8,7 +8,7 @@ from api.v1.auth.auth import Auth
 from api.v1.auth.basic_auth import BasicAuth
 from flask import Flask, jsonify, abort, request
 from flask_cors import (CORS, cross_origin)
-
+import os
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
